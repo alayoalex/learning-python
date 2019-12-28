63d7b3f277ab31d6381455e6e95a7ec02de8e1ee
Alexei Alayo <alexei.alayo@gmail.com>
2019-12-28 02:39:21 -0400

    ...

--- image_processing/code/image_processing.py
+++ image_processing/code/image_processing.py
@@ -1,15 +1,14 @@
 from __future__ import print_function
 from PIL import Image
 
-im = Image.open("image_processing/_MG_5192.jpg")
+im = Image.open("")
 print(im.format, im.size, im.mode)
 #im.show()
 
 #box = (100, 100, 400, 400)
 #region = im.crop(box)
-# region.show()
+#region.show()
 
 #r, g, b = im.split()
 #mm = Image.merge("RGB", (b, g, r))
 #mm.show()
-
