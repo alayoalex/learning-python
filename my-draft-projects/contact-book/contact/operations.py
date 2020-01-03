73dd493dc6c449adb10637d2efb25ce1fdeb5f52
Alexei Alayo <alexei.alayo@gmail.com>
2020-01-02 20:56:21 -0400

    Already done the save funcionality.

--- my-draft-projects/contact-book/contact/operations.py
+++ my-draft-projects/contact-book/contact/operations.py
@@ -1,6 +1,23 @@
+import os
+
+
+def counter():
+    if not os.path.exists("data/counter.txt"):
+        count = open("data/counter.txt", 'w')
+        count.write(str(1))
+        count.close()
+    else:
+        count = open("data/counter.txt", 'r')
+        c = int(count.read())
+        c += 1
+        countmore = open("data/counter.txt", 'w')
+        countmore.write(str(c))
+        return c
+        
+
 def save(*data):
-    datafile = open("data/data.txt", 'a')
-    datafile.write('\n')
+    c = counter()
+    datafile = open("data/data%s.txt" %(c), 'w')
     for i in range(len(data)):
         datafile.write(data[i] + '\n')
 
