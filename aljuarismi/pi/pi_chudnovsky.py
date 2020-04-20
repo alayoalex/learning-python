760cf725d47498a86dcd4305952c9f5baf758331
Alexei Alayo <alexei.alayo@gmail.com>
2020-04-19 20:08:23 -0500

    computing pi with chudnovsky algorithm

---
+++ aljuarismi/pi/pi_chudnovsky.py
@@ -0,0 +1,12 @@
+"""
+We have still got a long way to go though, and we'll have to improve both our 
+algorithm (formula) for π and our implementation.
+
+The current darling of the π world is the Chudnovsky algorithm which is similar 
+to the arctan formula but it converges much quicker. It is also rather complicated. 
+The formula itself is derived from one by Ramanjuan who's work was 
+extraordinary in the extreme.
+
+https://www.craig-wood.com/nick/articles/pi-chudnovsky/
+http://en.wikipedia.org/wiki/Chudnovsky_algorithm
+"""
